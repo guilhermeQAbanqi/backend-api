69d53fe263b9713fd1f91245097c569b32014b65
guilhermeQAbanqi <guilherme.domingos@banqi.com>
2022-06-28 19:14:24 -0300

    Refatorando projeto com login jwt

--- server.js
+++ server.js
@@ -1,32 +1,14 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-console.log(process.env.DATABASE_URL)
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
 const bp = require('body-parser')
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
+require("dotenv").config();
 
-connectDB().catch(err => console.log(err)); 
+const { API_PORT } = process.env;
+const port = process.env.PORT || API_PORT;
 
-// const PORT = 8081
-// const HOST = '0.0.0.0'
-
-async function connectDB() {
-await mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true, useUnifiedTopology: true
-})
-    .then(() => console.log("DB Connection Success"))
-    .catch((err) => console.log(err));
-}
-
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})  
-
-const routes = require('./routes/user_registration');
-const routesLogin = require('./routes/user_login');
-
-app.use('/api', routes)
-app.use('/api', routesLogin)
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
