025b1f6702e9d4fe5031d91441c599476bc20550
guilhermeQAbanqi <guilherme.domingos@banqi.com>
2022-07-07 14:44:18 -0300

    organizando rotas

--- routes/create_user.js
+++ routes/create_user.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
-require("./config/database").connect();
+require("../config/database").connect();
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const User = require("./model/user");
-const auth = require("./middleware/auth");
+const User = require("../model/user");
+const auth = require("../middleware/auth");
 
-const app = express();
+const create_user = express();
 
-app.use(express.json({ limit: "50mb" }));
+create_user.use(express.json({ limit: "50mb" }));
 
-app.post("/register", async (req, res) => {
+create_user.post("/register", async (req, res) => {
   try {
     // Get user input
     const { first_name, last_name, email, password } = req.body;
@@ -58,46 +58,12 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
-  try {
-    // Get user input
-    const { email, password } = req.body;
-
-    // Validate user input
-    if (!(email && password)) {
-      res.status(400).send("All input is required");
-    }
-    // Validate if user exist in our database
-    const user = await User.findOne({ email });
-
-    if (user && (await bcrypt.compare(password, user.password))) {
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-
-      // save user token
-      user.token = token;
-
-      // user
-      res.status(200).json(user);
-    }
-    res.status(400).send("Invalid Credentials");
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.get("/welcome", auth, (req, res) => {
+create_user.get("/welcome", auth, (req, res) => {
   res.status(200).send("Welcome 🙌 ");
 });
 
 // This should be the last route else any after it won't work
-app.use("*", (req, res) => {
+create_user.use("*", (req, res) => {
   res.status(404).json({
     success: "false",
     message: "Page not found",
@@ -108,4 +74,4 @@ app.use("*", (req, res) => {
   });
 });
 
-module.exports = app;
+module.exports = create_user;
---
+++ routes/login.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+require("../config/database").connect();
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../model/user");
+
+const login = express();
+
+login.post("/login", async (req, res) => {
+    try {
+        // Get user input
+        const { email, password } = req.body;
+
+        // Validate user input
+        if (!(email && password)) {
+            res.status(400).send("All input is required");
+        }
+        // Validate if user exist in our database
+        const user = await User.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "2h",
+                }
+            );
+
+            // save user token
+            user.token = token;
+
+            // user
+            res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+module.exports = login;
